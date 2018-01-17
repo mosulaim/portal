631656f19b6878101c41ddeb6682fef3edae4fad
mosulaim <mosulaim@gmail.com>
2018-01-17 21:57:49 +0100

    constring change

--- assets/gdb_constring.php
+++ assets/gdb_constring.php
@@ -9,6 +9,6 @@ define('DB_DATABASE', getenv('POSTGRESQL_DATABASE')); // database name
 define('DB_HOST', getenv('POSTGRESQL_SERVICE_HOST')); // db server
 define('DB_PORT', getenv('POSTGRESQL_SERVICE_PORT'));
 
- define('DB_CONNSTRING', 'host='.DB_HOST.' port='.DB_PORT.' dbname='.DB_DATABASE.' user='.DB_USER.' password='.DB_PASSWORD); // database name
+ define('DB_CONNSTRING', 'host='.DB_HOST.' port=5432 dbname=mapdb user=postgresql password=PgAdmin'); // database name
 
 ?>
